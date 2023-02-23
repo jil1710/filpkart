b8419b7b1132def35cd2511f260ed307e27515fb
rider <jil.p@simformsolutions.com>
2023-02-23 11:34:15 +0530

    Feat: Add Some Deals to grab it on & Add Footer
    Using SwiperJs library i added Some product list that help's user to grab the deals and add it on cart. In Last i finish it by adding footer on home page

--- index.js
+++ index.js
@@ -1,14 +1,93 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 6,
+    spaceBetween: 10,
     loop: true,
-
-    // Navigation arrows
+    fade: 'true',
+    grabCursor: 'true',
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        550:{
+            slidesPerView: 2,
+
+        },
+        990: {
+            slidesPerView: 3,
+        },
+        1400: {
+            slidesPerView: 4,
+        },
+        1500: {
+            slidesPerView: 5,
+        },
+        1600: {
+            slidesPerView: 6,
+        },
     },
   });
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    loop: true,
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        550:{
+            slidesPerView: 2,
+
+        },
+        990: {
+            slidesPerView: 3,
+        },
+        1400: {
+            slidesPerView: 4,
+        },
+        1500: {
+            slidesPerView: 5,
+        },
+        1600: {
+            slidesPerView: 6,
+        },
+    },
+  });
+
+var countDate = new Date("March 3, 2023 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("datetime").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s Left";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
 
 const toggleBtn = document.querySelector('.bar');
